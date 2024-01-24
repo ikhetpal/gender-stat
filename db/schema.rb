513d0dd3959605156e66704e88e95cbce73023ef
Ishwar <ikhetpal@gmail.com>
2024-01-25 00:12:50 +0530

    DailyRecord model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_24_181726) do
+ActiveRecord::Schema.define(version: 2024_01_24_184151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "daily_records", force: :cascade do |t|
+    t.date "date"
+    t.integer "male_count"
+    t.integer "female_count"
+    t.float "male_avg_age"
+    t.float "female_avg_age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "uuid"
