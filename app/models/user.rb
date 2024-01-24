205d53148de169f89f46a25fad84c83de1972808
Ishwar <ikhetpal@gmail.com>
2024-01-25 00:23:30 +0530

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	GENDERS = %w(male female)
+
+	validates :uuid, uniqueness: true
+	validates :gender, inclusion: { in: GENDERS }
+	validates :age, numericality: { greater_than: 0 }
 end
